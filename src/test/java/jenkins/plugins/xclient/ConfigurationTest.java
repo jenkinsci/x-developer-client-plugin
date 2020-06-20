2d37997e99cf5b1de04c48026904f2c0a01320e5
陈加兴 <jxchen@withfield.tech>
2020-06-20 18:30:29 +0800

    [HOSTING-938] fix hudson slave error

--- src/test/java/jenkins/plugins/xclient/ConfigurationTest.java
+++ src/test/java/jenkins/plugins/xclient/ConfigurationTest.java
@@ -1,11 +1,10 @@
 package jenkins.plugins.xclient;
 
-import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
-import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+import com.gargoylesoftware.htmlunit.html.*;
 import org.junit.*;
 import static org.junit.Assert.*;
-import org.jvnet.hudson.test.RestartableJenkinsRule;
+
+import org.jvnet.hudson.test.JenkinsRule;
 
 /**
  * @author Chen Jiaxing
@@ -14,45 +13,33 @@ import org.jvnet.hudson.test.RestartableJenkinsRule;
 public class ConfigurationTest {
 
     @Rule
-    public RestartableJenkinsRule rr = new RestartableJenkinsRule();
+    public JenkinsRule rule = new JenkinsRule();
 
     @Test
-    public void idUiAndStorage() {
-        rr.then(r -> {
-            assertNull("not set initially", Configuration.get().getAppId());
-            HtmlForm config = r.createWebClient().goTo("configure").getFormByName("config");
-            HtmlTextInput text = config.getInputByName("_.appId");
-            text.setText("hello");
-            r.submit(config);
-            assertEquals("global config page let us edit it", "hello", Configuration.get().getAppId());
-        });
-        rr.then(r -> {
-            assertEquals("still there after restart of Jenkins", "hello", Configuration.get().getAppId());
-        });
+    public void idUiAndStorage() throws Exception {
+        assertNull("not set initially", Configuration.get().getAppId());
+        HtmlForm config = rule.createWebClient().goTo("configure").getFormByName("config");
+        HtmlTextInput text = config.getInputByName("_.appId");
+        text.setText("hello");
+        rule.submit(config);
+        assertEquals("global config page let us edit it", "hello", Configuration.get().getAppId());
     }
 
     @Test
-    public void keyUiAndStorage() {
-        rr.then(r -> {
-            assertEquals("not set initially", "", Configuration.get().getAppKey());
-            HtmlForm config = r.createWebClient().goTo("configure").getFormByName("config");
-            HtmlPasswordInput text = config.getInputByName("_.appKey");
-            text.setText("appKey");
-            r.submit(config);
-            assertEquals("global config page let us edit it", "appKey", Configuration.get().getAppKey());
-        });
-        rr.then(r -> {
-            assertEquals("still there after restart of Jenkins.", "appKey", Configuration.get().getAppKey());
-        });
+    public void keyUiAndStorage() throws Exception {
+        assertEquals("not set initially", "", Configuration.get().getAppKey());
+        HtmlForm config = rule.createWebClient().goTo("configure").getFormByName("config");
+        HtmlPasswordInput text = config.getInputByName("_.appKey");
+        text.setText("appKey");
+        rule.submit(config);
+        assertEquals("global config page let us edit it", "appKey", Configuration.get().getAppKey());
     }
 
     @Test
     public void secretKey() {
-        rr.then(r -> {
-            String key = "5dbae4b5c150c63f9871ec23";
-            Configuration.get().setAppKey(key);
-            assertEquals(key, Configuration.get().getAppKey());
-        });
+        String key = "5dbae4b5c150c63f9871ec23";
+        Configuration.get().setAppKey(key);
+        assertEquals(key, Configuration.get().getAppKey());
     }
 
 }
